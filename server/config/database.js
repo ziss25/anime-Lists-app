906715dcf633b5d835b9c4f6ab1ebd66d47819c9
ziss aja <ziss2517@gmail.com>
2023-10-03 12:30:18 +0800

    init env server

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,8 @@ const Sequelize = require('sequelize');
 // more detail -- acount survingtesting@gmail.com
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'mysql',
+  dialect: process.env.DB,
+  dialectModule: require('mysql2'),
   port: process.env.DB_PORT,
 });
 
