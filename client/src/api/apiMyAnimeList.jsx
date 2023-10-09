74aea6d08084fc75977613e913782f48ad78f369
ziss aja <ziss2517@gmail.com>
2023-10-09 11:41:20 +0800

    lasted commit

--- client/src/api/apiMyAnimeList.jsx
+++ client/src/api/apiMyAnimeList.jsx
@@ -1,9 +1,7 @@
-import axios from 'axios';
 const maxRetries = 3; // Maximum number of retry attempts
 const delayBetweenRetries = 3000; // Delay in milliseconds before retrying (3 seconds)
 const baseUrl = 'https://api.jikan.moe/v4';
-const urlServer = 'https://animelist-server.vercel.app';
-// const urlServer = 'http://localhost:5000';
+const urlServer = 'https://soonflix-server-app.vercel.app';
 
 // promises handle -- limit request
 function makeRequestWithRetry(url, maxRetries, delay) {
